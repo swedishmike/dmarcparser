ca65d529405fe2c14d38baffcf8bd7350c60a7fa
Mike Eriksson <swedishmike@users.noreply.github.com>
2016-11-10 08:58:48 +0000

    Sorted imports etc.

--- dmarcparser.py
+++ dmarcparser.py
@@ -1,6 +1,9 @@
 from  __future__ import print_function
-
+from modules.imap_connect import connect_and_find_new_reports
+from modules.imap_connect import extract_files
 __version__ = "0.5"
 
 if __name__ == '__main__':
     print ("Dmarc Parser ver", __version__)
+    imap = connect_and_find_new_reports(verbose=True)
+    extract_files()
